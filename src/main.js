1bc66a72ae827e7d7b5cb8c25d102fbad85bf9d1
Mikhail Podanev <mikhailp@leadsmarket.com>
2019-08-23 18:08:23 +0300

    module4-task1 prepared

--- src/main.js
+++ src/main.js
@@ -3,47 +3,92 @@ import {createHeaderProfileTemplate} from './components/headerProfileTemplate';
 import {createMainNavigationTemplate} from './components/mainNavigationTemplate';
 import {createSortTemplate} from './components/sortTemplate';
 import {createFilmsTemplate} from './components/filmsTemplate';
-import {createFilmCardTemplate} from './components/filmCardTemplate';
+// import {createFilmCardTemplate} from './components/filmCardTemplate';
 import {createShowMoreBtnTemplate} from './components/showMoreBtnTemplate';
-import {createFilmDetailsTemplate} from './components/filmDetailsTemplate';
-import {getTaskData} from './taskData';
+// import {createFilmDetailsTemplate} from './components/filmDetailsTemplate';
+import {getFilmData} from './filmData';
 import {getFilters} from './filtersData';
+import Film from './components/film';
+import FilmPopup from './components/filmPopup';
+import {Position, render, unrender} from './utils';
 
-const headerElement = document.querySelector(`.header`);
-const mainElement = document.querySelector(`.main`);
+// const headerElement = document.querySelector(`.header`);
+// const mainElement = document.querySelector(`.main`);
+
+// const renderTemplates = (container, element, position, quantity = 1) => {
+//   new Array(quantity).fill(``).forEach(() => container.insertAdjacentHTML(position, element));
+// };
+
 const bodyElement = document.querySelector(`body`);
-
-const renderTemplates = (container, element, position, quantity = 1) => {
-  new Array(quantity).fill(``).forEach(() => container.insertAdjacentHTML(position, element));
-};
+const filmsContainer = document.querySelector(`.films-list__container`);
 const MAIN_FILMS_LIST_COUNT = 5;
 const ADDITIONAL_FILMS_LIST_COUNT = 2;
-const films = new Array(MAIN_FILMS_LIST_COUNT)
-  .fill(``)
-  .map(getTaskData)
-  .map(createFilmCardTemplate);
+// const films = new Array(MAIN_FILMS_LIST_COUNT)
+//   .fill(``)
+//   .map(getTaskData)
+//   .map(createFilmCardTemplate);
+const filmMocks = new Array(MAIN_FILMS_LIST_COUNT).fill(``).map(getFilmData);
 
-renderTemplates(headerElement, createHeaderSearch(), `beforeend`);
-renderTemplates(headerElement, createHeaderProfileTemplate(), `beforeend`);
-renderTemplates(mainElement, createMainNavigationTemplate(getFilters()), `beforeend`);
-renderTemplates(mainElement, createSortTemplate(), `beforeend`);
-renderTemplates(mainElement, createFilmsTemplate(), `beforeend`);
+const renderFilm = (filmMock) => {
+  const film = new Film(filmMock);
+  const filmPopup = new FilmPopup(filmMock);
 
-const filmsListContainerElement = document.querySelectorAll(`.films-list__container`);
-const filmsListContainerFirst = filmsListContainerElement[0];
-const filmsListContainerSecond = filmsListContainerElement[1];
-const filmsListContainerThird = filmsListContainerElement[2];
-const filmListElemetn = document.querySelector(`.films-list`);
+  const onEscKeyDown = (evt) => {
+    if (evt.key === `Escape` || evt.key === `Esc`) {
+      // TODO: сделать правильное удаление попапа
+      // filmsContainer.replaceChild(film.getElement(), filmPopup.getElement());
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
 
-renderTemplates(filmsListContainerFirst, films.join(``), `beforeend`);
-renderTemplates(filmsListContainerSecond, films.slice(-1).join(``), `beforeend`, ADDITIONAL_FILMS_LIST_COUNT);
-renderTemplates(filmsListContainerThird, films.slice(0, 1).join(``), `beforeend`, ADDITIONAL_FILMS_LIST_COUNT);
-renderTemplates(filmListElemetn, createShowMoreBtnTemplate(), `beforeend`);
-renderTemplates(bodyElement, createFilmDetailsTemplate(getTaskData()), `beforeend`);
+  film.getElement().querySelector(`.film-card__comments`)
+    .addEventListener(`click`, () => {
+      render(bodyElement, filmPopup.getElement(), Position.BEFOREEND);
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
 
-const filmsListShowMoreBtn = document.querySelector(`.films-list__show-more`);
+  // TODO: Убирать возможность закрытия попапа при добавлении коментария
+  // filmPopup.getElement().querySelector(`textarea`)
+  //   .addEventListener(`focus`, () => {
+  //     document.removeEventListener(`keydown`, onEscKeyDown);
+  //   });
 
-filmsListShowMoreBtn.addEventListener(`click`, (evt) => {
-  renderTemplates(filmsListContainerFirst, films.join(``), `beforeend`);
-  filmsListShowMoreBtn.remove();
-});
+  // TODO:
+  // filmPopup.getElement().querySelector(`textarea`)
+  //   .addEventListener(`blur`, () => {
+  //     document.addEventListener(`keydown`, onEscKeyDown);
+  //   });
+
+  filmPopup.getElement().querySelector(`.card__save`)
+    .addEventListener(`click`, () => {
+      filmsContainer.replaceChild(film.getElement(), filmPopup.getElement());
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+  render(filmsContainer, film.getElement(), Position.BEFOREEND);
+};
+
+// renderTemplates(headerElement, createHeaderSearch(), `beforeend`);
+// renderTemplates(headerElement, createHeaderProfileTemplate(), `beforeend`);
+// renderTemplates(mainElement, createMainNavigationTemplate(getFilters()), `beforeend`);
+// renderTemplates(mainElement, createSortTemplate(), `beforeend`);
+// renderTemplates(mainElement, createFilmsTemplate(), `beforeend`);
+
+// const filmsListContainerElement = document.querySelectorAll(`.films-list__container`);
+// const filmsListContainerFirst = filmsListContainerElement[0];
+// const filmsListContainerSecond = filmsListContainerElement[1];
+// const filmsListContainerThird = filmsListContainerElement[2];
+// const filmListElemetn = document.querySelector(`.films-list`);
+
+// renderTemplates(filmsListContainerFirst, films.join(``), `beforeend`);
+// renderTemplates(filmsListContainerSecond, films.slice(-1).join(``), `beforeend`, ADDITIONAL_FILMS_LIST_COUNT);
+// renderTemplates(filmsListContainerThird, films.slice(0, 1).join(``), `beforeend`, ADDITIONAL_FILMS_LIST_COUNT);
+// renderTemplates(filmListElemetn, createShowMoreBtnTemplate(), `beforeend`);
+// renderTemplates(bodyElement, createFilmDetailsTemplate(getFilmData()), `beforeend`);
+
+// const filmsListShowMoreBtn = document.querySelector(`.films-list__show-more`);
+
+// filmsListShowMoreBtn.addEventListener(`click`, (evt) => {
+//   renderTemplates(filmsListContainerFirst, films.join(``), `beforeend`);
+//   filmsListShowMoreBtn.remove();
+// });
