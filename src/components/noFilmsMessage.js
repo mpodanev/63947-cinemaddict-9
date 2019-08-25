07d6678a22b74ca723af10d61f17f9652bd6bc78
Mikhail Podanev <mpodanev@MacBook-Pro-Mikhail.local>
2019-08-25 13:05:00 +0300

    module4-task2

---
+++ src/components/noFilmsMessage.js
@@ -0,0 +1,25 @@
+import {createElement} from '../utils';
+
+export default class NoFilmsMessage {
+  constructor() {
+    this._element = null;
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return `<div class="no-result">
+    There is no movies for your request.
+  </div>`;
+  }
+}
